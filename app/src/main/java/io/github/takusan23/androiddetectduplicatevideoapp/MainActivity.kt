59bb982351e854a09ce814b948f8cef954120abc
takusan23 <takumu1200@gmail.com>
2024-10-04 03:24:54 +0900

    プレイヤーを開くのとゴミ箱に移動ができるボトムシート

--- app/src/main/java/io/github/takusan23/androiddetectduplicatevideoapp/MainActivity.kt
+++ app/src/main/java/io/github/takusan23/androiddetectduplicatevideoapp/MainActivity.kt
@@ -1,11 +1,13 @@
 package io.github.takusan23.androiddetectduplicatevideoapp
 
+import android.app.Activity
 import android.content.Intent
 import android.media.MediaCodec
 import android.media.MediaMetadataRetriever
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Size
 import android.widget.Toast
 import androidx.activity.ComponentActivity
@@ -15,19 +17,25 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -35,6 +43,7 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
@@ -108,12 +117,12 @@ private fun MainScreen() {
             // リストに同時にでアクセスさせないように mutex
             val listLock = Mutex()
             // 並列処理数を制限。ハードウェアデコーダーは同時起動上限がある。多分16個くらい
-            val semaphore = Semaphore(8)
+            val semaphore = Semaphore(16)
             // 処理結果
             val videoFrameHashDataList = arrayListOf<VideoFrameHashData>()
 
             // TODO 時間がかかりすぎるので上限
-            videoUriList.take(1_000).map { uri ->
+            videoUriList.take(500).map { uri ->
                 println("start $uri")
 
                 // 並列ではしらす
@@ -241,6 +250,7 @@ private fun MainScreen() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun VideoThumbnailImage(
     modifier: Modifier = Modifier,
@@ -248,6 +258,7 @@ private fun VideoThumbnailImage(
 ) {
     val context = LocalContext.current
     val thumbnailBitmap = remember { mutableStateOf<ImageBitmap?>(null) }
+    val isShowBottomSheet = remember { mutableStateOf(false) }
 
     LaunchedEffect(key1 = uri) {
         withContext(Dispatchers.IO) {
@@ -255,15 +266,45 @@ private fun VideoThumbnailImage(
         }
     }
 
+    // 削除とかできるボトムシート
+    if (isShowBottomSheet.value) {
+        ModalBottomSheet(onDismissRequest = { isShowBottomSheet.value = false }) {
+            TextButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { context.startActivity(Intent(Intent.ACTION_VIEW, uri)) }
+            ) {
+                Text(text = "動画プレイヤーを開く")
+            }
+            TextButton(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = {
+                    // ゴミ箱に移動リクエストの Intent をつくる
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        val intent = MediaStore.createTrashRequest(context.contentResolver, listOf(uri), true)
+                        (context as? Activity)?.startIntentSenderForResult(intent.intentSender, 0, null, 0, 0, 0)
+                    } else {
+                        // TODO 古い Android は未実装
+                    }
+                }
+            ) {
+                Text(text = "ゴミ箱に移動（Google フォトから復元できます）")
+            }
+        }
+    }
+
     if (thumbnailBitmap.value != null) {
         Image(
-            modifier = modifier.clickable {
-                val intent = Intent(Intent.ACTION_VIEW, uri)
-                context.startActivity(intent)
-            },
+            modifier = modifier.clickable { isShowBottomSheet.value = !isShowBottomSheet.value },
             bitmap = thumbnailBitmap.value!!,
             contentDescription = null
         )
+    } else {
+        Box(
+            modifier = modifier,
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
     }
 }
 
